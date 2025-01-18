6a107d73b563257bd2c7f4d36bc49ad4e8fc319d
IvanBelousov72908 <105873927+IvanBelousov72908@users.noreply.github.com>
2025-01-18 20:32:32 +0800

    Made changes to the music.

--- music.js
+++ music.js
@@ -4,13 +4,33 @@ class MusicPlayer {
             this.audio = new Audio('Underclocked - Eric Skiff.mp3');
             this.audio.loop = true;
             this.audio.volume = 0.5;
+            
+            // Store the current time before page unload
+            window.addEventListener('beforeunload', () => {
+                sessionStorage.setItem('musicTime', this.audio.currentTime);
+            });
+
             MusicPlayer.instance = this;
         }
         return MusicPlayer.instance;
     }
 
     play() {
-        this.audio.play();
+        // Try to resume from the last stored position
+        const lastTime = sessionStorage.getItem('musicTime');
+        if (lastTime) {
+            this.audio.currentTime = parseFloat(lastTime);
+        }
+        
+        // Start playing
+        this.audio.play().catch(error => {
+            console.log("Playback failed:", error);
+        });
+        
+        // Periodically store the current time
+        setInterval(() => {
+            sessionStorage.setItem('musicTime', this.audio.currentTime);
+        }, 1000);
     }
 
     getCurrentTime() {
